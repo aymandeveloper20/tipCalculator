3839ebfbc044826110c4b93c4362b58ac69ef032
aymandeveloper20 <aymangithub20@outlook.com>
2024-09-22 19:26:49 +0300

    calculation Improvement

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let btn = document.querySelector(".btn");
 
 btn.addEventListener("click", (event) => {
   event.preventDefault();
-  let billAmount = parseInt(document.querySelector("#billAmount").value);
-  let tipPercentage = parseInt(document.querySelector("#tipPercentage").value);
-  let tip = tipPercentage / 100;
-  let total = billAmount * tip;
-  document.querySelector(".display span").innerHTML = `$${total}`;
+  let billAmount = parseFloat(document.querySelector("#billAmount").value);
+  let tipPercentage = parseFloat(
+    document.querySelector("#tipPercentage").value
+  );
+  let tip = (billAmount * tipPercentage) / 100;
+  let total = billAmount + tip;
+  document.querySelector(".display span").innerHTML = `$${total.toFixed(2)}`;
 });
